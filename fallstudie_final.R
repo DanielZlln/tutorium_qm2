8273c87592be29cc317d73e631eae9fe36ef85e0
DanielZlln <daniel_zellner@icloud.com>
2023-05-20 15:05:39 +0200

    corr

--- fallstudie_final.R
+++ fallstudie_final.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(purrr)
 library(corrplot)
 library(corrgram)
+library(recipes)
+library(corrgram)
+library(corrplot)
 
 source("ackerland.R")
 source("beschaeftigte.R")
@@ -9,7 +12,7 @@ source("bevoelkerung.R")
 source("bodenflaeche.R")
 source("wahlbeteiligung.R")
 source("tourismus.R")
-source("siedlungsfläche.R")
+source("siedlungsflaeche.R")
 
 
 df_list <- list(bodenflaeche, siedlungsflaeche, ackerland, beschaeftigte, bevoeklerung, 
@@ -18,13 +21,33 @@ df_list <- list(bodenflaeche, siedlungsflaeche, ackerland, beschaeftigte, bevoek
 fallstudie <- Reduce(function(x,y) merge(x,y, by = c("ID","Kreis"), all = TRUE),
        df_list)
 
-neu_fallstudie <- fallstudie
-
-fallstudie <- for(i in 1:ncol(fallstudie)) {
+for(i in 1:ncol(fallstudie)) {
   if (colnames(fallstudie[i]) == "Kreis" || colnames(fallstudie[i]) == "ID") {
-    neu_fallstudie[,i] <- fallstudie[,i]
+    fallstudie[,i] <- fallstudie[,i]
   } else {
-    neu_fallstudie[,i] <- as.numeric(fallstudie[,i]) 
+    fallstudie[,i] <- as.numeric(fallstudie[,i]) 
   }
 }
 
+str(fallstudie)
+
+rec <- recipe(~., data = fallstudie)
+?step_impute_knn
+
+ratio_recipe <- rec %>% 
+  step_impute_knn(all_predictors(), neighbors = 3)
+
+ratio_recipe_2 <- prep(ratio_recipe)
+
+fallstudie <- bake(ratio_recipe_2, new_data = NULL)
+
+summary(fallstudie)
+
+fallstudie_num <- fallstudie[sapply(fallstudie, is.numeric)]
+
+sapply(fallstudie_num, class)
+
+cor_matrix <- cor(fallstudie_num)
+
+corrgram(cor_matrix)
+corrplot(cor_matrix, order = "hclust")
